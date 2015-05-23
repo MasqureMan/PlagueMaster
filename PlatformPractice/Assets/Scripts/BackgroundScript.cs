3659a7c959df5a786efd90d389f13d83052ed858
MasqureMan <langston.lewis@loop.colum.edu>
2015-05-23 03:48:36 -0500

    Cleanup commit
    Getting it to pull and run

---
+++ PlatformPractice/Assets/Scripts/BackgroundScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScript : MonoBehaviour {
+
+public Texture2D Background;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	private void OnGUI() {
+		//Uses the background if it's not null
+		if (Background != null)
+		{
+			GUI.DrawTexture(new Rect(0,0, Screen.width, Screen.height), Background); 
+		}
+}
+}
---
+++ PlatformPractice/Assets/Scripts/MoveonPlatform.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter (other : Collider) 
+
+{ other.transform.parent = gameObject.transform; } 
+
+function OnTriggerExit (other : Collider) 
+
+{ other.transform.parent = null; }
